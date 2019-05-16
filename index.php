2f0aa58eac807cda7e4772958deb76aa7aee350c
Stefan Ciuchi <stefan.ciuchi@emag.ro>
2019-05-16 17:45:48 +0300

    modified html - work in progress

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/web/views');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('ecb/ecb_converter.html.twig');
+echo $twig->render('partials/converter.html.twig');
 
 
 
-$url = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+$url = 'https://www.partials.europa.eu/stats/eurofxref/eurofxref-daily.xml';
 $xml = file_get_contents($url);
 var_dump($xml);
