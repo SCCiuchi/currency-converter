189b76768181cd73cabc93be3913a467001b6e27
Stefan Ciuchi <stefan.ciuchi@emag.ro>
2019-05-21 18:37:09 +0300

    fixed twig issue

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/views');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('index.html.twig');
+echo $twig->render('converter.html.twig');
 
 //$debug = new \App\Controllers\DisplayController(new \App\Converter\Ecb(new Config\RateReader\XMLReader()));
 //$debug->displayRate();
